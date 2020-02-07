d7494054e4b3abb0c74a2b226476ad0f6858b4ac
이성일 <modolee@soblo.io>
2020-02-07 18:22:33 +0900

    Class 사용

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-    name:string,
-    age:number,
-    gender:string
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "modolee",
-    age: 35,
-    gender: "male"
-};
+const modolee = new Human("modolee", 35, "male");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(modolee));
 
 export {};
