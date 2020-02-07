70c84cc219e35a9abed47379213e15913c26b968
이성일 <modolee@soblo.io>
2020-02-07 18:39:54 +0900

    블록체인 뼈대 작업

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,44 @@
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
+import * as CryptoJS from "crypto-js";
 
-    constructor(name: string, age: number, gender: string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const modolee = new Human("modolee", 35, "male");
+const genesisBlock: Block = new Block(
+    0, "20200000000222", "",
+    "This is First Block of blockchain", 123456);
 
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-};
+let blockchain: Block[] = [genesisBlock];
 
-console.log(sayHi(modolee));
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
